894948a125ea954f2f4f372bd3c889f7f76f587f
SwethaHosamani <swethach33@gmail.com>
2023-10-01 11:54:01 +0530

    database seeded properly

--- Program.cs
+++ Program.cs
@@ -1,21 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DatabaseSeeder.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using StudentProject.Models;
 
-// Console.WriteLine("Hello, World!");
-using System;
+// // Console.WriteLine("Hello, World!");
+// using System;
 
-namespace DatabaseSeeder
+// namespace DatabaseSeeder
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Seeding database...");
+//             var serviceProvider = new ServiceCollection().AddDbContext<TestDemoContext>().BuildServiceProvider();
+//             Seeder.Initialize(serviceProvider);
+//         }
+//     }
+// }
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         using (var context = new TestDemoContext())
+//         {
+//             Seeder.Initialize(context);
+//         }
+//     }
+// }
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var options = new DbContextOptionsBuilder<TestDemoContext>()
+            .UseSqlServer("Server=DESKTOP-RDT234N\\SQLEXPRESS;Database=TestDemo;Trusted_Connection=True;Integrated Security=True;TrustServerCertificate=True;")
+            .Options;
+
+        using (var context = new TestDemoContext(options))
         {
-            Console.WriteLine("Seeding database...");
-            var serviceProvider = new ServiceCollection().AddDbContext<TestDemoContext>().BuildServiceProvider();
-            Seeder.Initialize(serviceProvider);
+            Seeder.Initialize(context);
         }
     }
 }
-
