8a086a853290689c232244ffe5e08a0919f538d7
SwethaHosamani <swethach33@gmail.com>
2023-10-01 11:34:40 +0530

    added seeding files

---
+++ Data/Seeder.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StudentProject.Models;
+
+namespace DatabaseSeeder.Data
+{
+    public static class Seeder
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new TestDemoContext(
+                serviceProvider.GetRequiredService<DbContextOptions<TestDemoContext>>()))
+            {
+                if (context.StudentData.Any())
+                {
+                    Console.WriteLine("Database already seeded.");
+                    return; 
+                }
+
+                var students = new StudentDatum[]
+                {
+                    new StudentDatum
+                    {
+                        StudentId = "STDN00001",
+                        Gender = "F",
+                        Nationlity = "USA",
+                        PlaceOfBirth = "New York",
+                        StageId = "lowerlevel",
+                        GradeId = "G01",
+                        SectionId = "A",
+                        Topic = "Math",
+                        Semester = "F",
+                        Relation = "Mother",
+                        RaisedHands = 10,
+                        VisitedResources = 20,
+                        AnnouncementsView = 5,
+                        Discussion = 15,
+                        ParentAnsweringSurvey = "Yes",
+                        ParentschoolSatisfaction = "Good",
+                        StudentAbsenceDays = "Under-7",
+                        StudentMarks = 85,
+                        Classes = "M"
+                    },
+                    // Add more students as needed
+                };
+
+                context.StudentData.AddRange(students);
+                context.SaveChanges();
+                Console.WriteLine("Database seeded successfully.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DatabaseSeeder.Data;
+using Microsoft.Extensions.DependencyInjection;
+using StudentProject.Models;
+
+// Console.WriteLine("Hello, World!");
+using System;
+
+namespace DatabaseSeeder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seeding database...");
+            var serviceProvider = new ServiceCollection().AddDbContext<TestDemoContext>().BuildServiceProvider();
+            Seeder.Initialize(serviceProvider);
+        }
+    }
+}
+
